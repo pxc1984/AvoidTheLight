c858630f005aa1682e897f48444027f9e651b051
JDtrimble <igor.mamaev1@gmail.com>
2023-01-15 20:48:22 +0300

    UNOPTIMAL, BUT WORKING

--- Enemy.py
+++ Enemy.py
@@ -25,7 +25,7 @@ class Enemy(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(x=x * CONSTANTS['SCALE'], y=y * CONSTANTS['SCALE'])
         self.x = x
         self.y = y
-        self.brightness = CONSTANTS['SCALE'] * 3
+        self.brightness = CONSTANTS['SCALE'] * 9    
         self.move_speed = {  # сделать зависимость от кадров
             'x': round(CONSTANTS['WIDTH'] * 0.15 / CONSTANTS['FPS']),
             'y': round(CONSTANTS['HEIGHT'] * 0.25 / CONSTANTS['FPS'])
@@ -101,7 +101,7 @@ class Enemy(pygame.sprite.Sprite):
         #         self.current_speed['x'], self.current_speed['y'] = -1, 1
         #     if self.path['direction'] == 8:  # 8 - left
         #         self.current_speed['x'], self.current_speed['y'] = -1, 0
-        # elif self.path['active'] < CONSTANTS['SCALE']:  # есть путь, проверка выполнился ли путь
+        # elif self.path['active'] < 64:  # есть путь, проверка выполнился ли путь
         #     self.path['active'] += 1
 
         if keys[pygame.K_LEFT]:
@@ -124,153 +124,111 @@ class Enemy(pygame.sprite.Sprite):
 class Light(pygame.sprite.Sprite):
     def __init__(self, x, y, enemy: Enemy):
         pygame.sprite.Sprite.__init__(self)
-
         self.image = pygame.surface.Surface((enemy.brightness*2, enemy.brightness*2))
-        self.image.fill((0, 0, 0))
-        for i, color in enumerate(COLORS['saturation_colors']):
-            pygame.draw.circle(self.image, color, (enemy.brightness, enemy.brightness), enemy.brightness - i*5)
+        self.image.fill(COLORS['background_color'])
         pygame.draw.circle(self.image, (255, 255, 255), (enemy.brightness, enemy.brightness), enemy.brightness)
         self.rect = self.image.get_rect(x=x, y=y)
 
-        self.mask_image = pygame.surface.Surface((enemy.brightness*2, enemy.brightness*2))
-        pygame.draw.circle(self.mask_image, (255, 255, 255), (enemy.brightness, enemy.brightness), enemy.brightness)
-        self.mask_image.set_colorkey((0, 0, 0))
-        self.mask = pygame.mask.from_surface(self.mask_image)
-
     def update(self, surface: pygame.surface.Surface, enemy: Enemy, level: pygame.sprite.Group):
-        self.generate_mask(surface, level, enemy)
         self.draw(surface, enemy)
-        # self.redraw(surface, level, enemy)
+        self.redraw(surface, level, enemy)
 
-    def draw(self, surface: pygame.surface.Surface, enemy: Enemy):
+    def draw(self, surface, enemy: Enemy):
         self.rect.center = enemy.rect.center
         surface.blit(self.image, self.rect)
 
-# Основной задачей переписывания этой функции можно считать маски, по которым герой должен взаимодействовать с врагом и необходимость в плавном исчезании света
-# Реализовываться это всё будет с помощью создания маски, по которой уже и будет рисоваться изображение и проверять коллизию герой
-    # def redraw(self, surface: pygame.surface.Surface, level: pygame.sprite.Group, enemy: Enemy):
-    #     collided_tiles = []
-    #     for tile in level:
-    #         if pygame.sprite.collide_rect(self, tile):
-    #             collided_points = []
-    #             if self.rect.collidepoint(tile.rect.topright):
-    #                 collided_points.append(tile.rect.topright)
-    #             if self.rect.collidepoint(tile.rect.topleft):
-    #                 collided_points.append(tile.rect.topleft)
-    #             if self.rect.collidepoint(tile.rect.bottomright):
-    #                 collided_points.append(tile.rect.bottomright)
-    #             if self.rect.collidepoint(tile.rect.bottomleft):
-    #                 collided_points.append(tile.rect.bottomleft)
-    #             collided_tiles.append(collided_points)
-    #     collided_points = []
-    #     for points in collided_tiles:
-    #         exit_value = sorted(points, key=lambda x: round(math.atan2(x[1] - self.rect.centery, x[0] - self.rect.centerx), 4))
-    #         # collided_points.append((exit_value[0], exit_value[-1]))  # Нужные точки уже отсортированные
-    #         pygame.draw.polygon(
-    #             surface, 
-    #             COLORS['background_color'], 
-    #             (exit_value[-1], 
-    #             exit_value[0], 
-    #             self.count_iterable(exit_value[0]), 
-    #             self.count_iterable(exit_value[-1])), 
-    #             # width = 10,
-    #             )
-    #         # pygame.draw.circle(surface, (255, 0, 0), (exit_value[0]), 5.0)
-    #         # pygame.draw.circle(surface, (0, 255, 0), (exit_value[-1]), 5.0)
-    #         # pygame.draw.circle(surface, (255, 0, 0), self.count_iterable(exit_value[-1]), 5.0)
-    #         # pygame.draw.circle(surface, (0, 255, 0), self.count_iterable(exit_value[0]), 5.0)
-        
-    def generate_mask(self, surface: pygame.surface.Surface, level: pygame.sprite.Group, enemy: Enemy):
+    def redraw(self, surface: pygame.surface.Surface, level: pygame.sprite.Group, enemy: Enemy):
         collided_tiles = []
         for tile in level:
             if pygame.sprite.collide_rect(self, tile):
                 collided_points = []
                 if self.rect.collidepoint(tile.rect.topright):
-                    collided_points.append((tile.rect.topright, round(math.atan2(tile.rect.topright[1] - self.rect.centery, tile.rect.topright[0] - self.rect.centerx), 6)))
+                    collided_points.append(tile.rect.topright)
                 if self.rect.collidepoint(tile.rect.topleft):
-                    collided_points.append((tile.rect.topleft, round(math.atan2(tile.rect.topleft[1] - self.rect.centery, tile.rect.topleft[0] - self.rect.centerx), 6)))
+                    collided_points.append(tile.rect.topleft)
                 if self.rect.collidepoint(tile.rect.bottomright):
-                    collided_points.append((tile.rect.bottomright, round(math.atan2(tile.rect.bottomright[1] - self.rect.centery, tile.rect.bottomright[0] - self.rect.centerx), 6)))
-                if self.rect.collidepoint(tile.rect.bottomleft):                        
-                    collided_points.append((tile.rect.bottomleft, round(math.atan2(tile.rect.bottomleft[1] - self.rect.centery, tile.rect.bottomleft[0] - self.rect.centerx), 6)))
+                    collided_points.append(tile.rect.bottomright)
+                if self.rect.collidepoint(tile.rect.bottomleft):
+                    collided_points.append(tile.rect.bottomleft)
                 collided_tiles.append(collided_points)
         collided_points = []
-        exit_values = []
         for points in collided_tiles:
-            s = sorted(points, key=lambda x: x[1])
-            exit_values.append((s[0], s[-1]))
+            exit_value = sorted(points, key=lambda x: math.atan2(x[1] - self.rect.centery, x[0] - self.rect.centerx))
+            # collided_points.append((exit_value[0], exit_value[-1]))  # Нужные точки уже отсортированные
+            pygame.draw.polygon(
+                surface, 
+                COLORS['background_color'], 
+                (exit_value[-1], 
+                exit_value[0], 
+                self.count_iterable(exit_value[0]), 
+                self.count_iterable(exit_value[-1])), 
+                # width = 10,
+                )
+            # pygame.draw.circle(surface, (255, 0, 0), (exit_value[0]), 5.0)
+            # pygame.draw.circle(surface, (0, 255, 0), (exit_value[-1]), 5.0)
+            # pygame.draw.circle(surface, (255, 0, 0), self.count_iterable(exit_value[-1]), 5.0)
+            # pygame.draw.circle(surface, (0, 255, 0), self.count_iterable(exit_value[0]), 5.0)
         
-        self.mask_image = pygame.surface.Surface((enemy.brightness * 2, enemy.brightness * 2))
-        self.mask_image.fill((0, 0, 0))
-        if len(exit_values) == 0:
-            pygame.draw.circle(self.mask_image, (255, 255, 255), (enemy.brightness, enemy.brightness), enemy.brightness)
-        else:
-            if len(exit_values) == 1:
-                pygame.draw.arc(self.mask_image, (255, 255, 255), (0, 0, enemy.brightness * 2, enemy.brightness * 2), exit_values[0][0][1], exit_values[0][1][1])
-                pygame.draw.polygon(self.mask_image, (255, 255, 255), ())
+
+    def count_iterable(self, value):
+        # interception_with_y, interception_with_x
+
+        # try:
+        #     inter_with_x = (-self.rect.centery*(value[0] - self.rect.centerx)/(value[1] - self.rect.centery)) + self.rect.centerx
+        # except ZeroDivisionError:
+        #     return [0, self.rect.centery]
+        # try:
+        #     inter_with_y = (-self.rect.centerx*(value[1] - self.rect.centery))/(value[1] - self.rect.centerx) + self.rect.centery
+        # except ZeroDivisionError:
+        #     return [self.rect.centerx, 0]
+        # # Я нашел ошибку в свете, он ищет ближайшее расстояние, а надо следующее
+        # if math.sqrt((self.rect.centerx - inter_with_x)**2 + (self.rect.centery)**2) <= math.sqrt((self.rect.centerx)**2 + (self.rect.centery - inter_with_y)**2):
+        #     return [inter_with_x, 0]
+        # else:
+        #     return [0, inter_with_y]
+
+        # Наша прямоя имеет вид 
+        # (x - self.rect.centerx)/(value[0] - self.rect.centerx) = (y - self.rect.centery)/(value[1] - self.rect.centery)
+        # Надо найти точки пересечения ее с прямыми {
+        # x = 0
+        # x = CONSTANTS['WIDTH']
+        # y = 0
+        # y = CONSTANTS['HEIGHT']
+        # }
+        # k = (value[0] - self.rect.centerx)/(x - self.rect.centerx)
+
+        x1, y1, x2, y2 = self.rect.centerx, self.rect.centery, value[0], value[1]
+        
+        try:
+            x0 = -y1*(x2-x1)/(y2-y1) + x1 # подставляю y = 0
+            xh = (CONSTANTS['HEIGHT']-y1)*(x2-x1)/(y2-y1) + x1  # подставляю y = height
+            if (x2 - x1)/(x0 - x1) > 0:
+                xf = (x0, 0)
             else:
-                for i in range(len(exit_values) - 1):
-                    pygame.draw.arc(self.mask_image, (255, 255, 255), (0, 0, enemy.brightness * 2, enemy.brightness * 2), exit_values[i][1][1], exit_values[i + 1][0][1])
-            pygame.draw.arc(self.mask_image, (255, 255, 255), (0, 0, enemy.brightness * 2, enemy.brightness * 2), exit_values[-1][1][1], exit_values[0][0][1])
-        self.mask_image.set_colorkey((0, 0, 0))
-        self.mask = pygame.mask.from_surface(self.mask_image)
-
-
-    # def count_iterable(self, value):
-    #     # interception_with_y, interception_with_x
-
-    #     # try:
-    #     #     inter_with_x = (-self.rect.centery*(value[0] - self.rect.centerx)/(value[1] - self.rect.centery)) + self.rect.centerx
-    #     # except ZeroDivisionError:
-    #     #     return [0, self.rect.centery]
-    #     # try:
-    #     #     inter_with_y = (-self.rect.centerx*(value[1] - self.rect.centery))/(value[1] - self.rect.centerx) + self.rect.centery
-    #     # except ZeroDivisionError:
-    #     #     return [self.rect.centerx, 0]
-    #     # # Я нашел ошибку в свете, он ищет ближайшее расстояние, а надо следующее
-    #     # if math.sqrt((self.rect.centerx - inter_with_x)**2 + (self.rect.centery)**2) <= math.sqrt((self.rect.centerx)**2 + (self.rect.centery - inter_with_y)**2):
-    #     #     return [inter_with_x, 0]
-    #     # else:
-    #     #     return [0, inter_with_y]
-
-    #     # Наша прямоя имеет вид 
-    #     # (x - self.rect.centerx)/(value[0] - self.rect.centerx) = (y - self.rect.centery)/(value[1] - self.rect.centery)
-    #     # Надо найти точки пересечения ее с прямыми {
-    #     # x = 0
-    #     # x = CONSTANTS['WIDTH']
-    #     # y = 0
-    #     # y = CONSTANTS['HEIGHT']
-    #     # }
-    #     # k = (value[0] - self.rect.centerx)/(x - self.rect.centerx)
-
-    #     x1, y1, x2, y2 = self.rect.centerx, self.rect.centery, value[0], value[1]
+                xf = (xh, CONSTANTS['HEIGHT'])
+        except ZeroDivisionError:
+            xf = (y1, 0)
         
-    #     try:
-    #         x0 = -y1*(x2-x1)/(y2-y1) + x1 # подставляю y = 0
-    #         xh = (CONSTANTS['HEIGHT']-y1)*(x2-x1)/(y2-y1) + x1  # подставляю y = height
-    #         if (x2 - self.rect.centerx)/(x0 - self.rect.centerx) >= 0:
-    #             xf = (x0, 0)
-    #         else:
-    #             xf = (xh, CONSTANTS['HEIGHT'])
-    #     except ZeroDivisionError:
-    #         xf = (x1, 0)
+        try:
+            y0 = -x1*(y2-y1)/(x2-x1) + y1  # подставляю x = 0
+            yw = (CONSTANTS['WIDTH']-x1)*(y2-y1)/(x2-x1) + y1  # подставляю x = width
+            if (y2 - y1)/(y0 - y1) > 0:
+                yf = (0, y0)
+            else:
+                yf = (CONSTANTS['WIDTH'], yw)
+        except ZeroDivisionError:
+            yf = (0, x1)
         
-    #     try:
-    #         y0 = -x1*(y2-y1)/(x2-x1) + y1  # подставляю x = 0
-    #         yw = (CONSTANTS['WIDTH']-x1)*(y2-y1)/(x2-x1) + y1  # подставляю x = width
-    #         if (y2 - y1)/(y0 - y1) >= 0:
-    #             yf = (0, y0)
-    #         else:
-    #             yf = (CONSTANTS['WIDTH'], yw)
-    #     except ZeroDivisionError:
-    #         yf = (0, y1)
         
-    #     # if math.sqrt((self.rect.centerx - xf[0])**2 + (self.rect.centery - xf[1])**2) <= math.sqrt((self.rect.centerx - yf[0])**2 + (self.rect.centery - yf[1])**2):
-    #     #     return xf
-    #     # else:
-    #     #     return yf
+        # if math.sqrt((self.rect.centerx - xf[0])**2 + (self.rect.centery - xf[1])**2) <= math.sqrt((self.rect.centerx - yf[0])**2 + (self.rect.centery - yf[1])**2):
+        #     return xf
+        # else:
+        #     return yf
         
-    #     if self.rect.centerx - xf[0] + self.rect.centery - xf[1] >= self.rect.centerx - yf[0] + self.rect.centery - yf[1]:
-    #         return xf
-    #     else:
-    #         return yf
+        if self.rect.centerx - xf[0] + self.rect.centery - xf[1] >= self.rect.centerx - yf[0] + self.rect.centery - yf[1]:
+            return xf
+        else:
+            return yf
+        
+
+
